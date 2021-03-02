9c6c6a020ae6f9a00922e467d0ee105b77e51257
Kristoff Finley <contentconsuming@gmail.com>
2021-03-01 21:20:52 -0700

    Reformated File, more readable

---
+++ Fractal_Visualization.py
@@ -0,0 +1,50 @@
+#Kristoff Finley
+#I followed the excellent tutorial from Tariq Rashid's "Make Your Own Mandelbrot" book
+import numpy as np
+import matplotlib.pyplot as plt
+
+#calculates whether point c diverges after maxiter iterations
+#using the function z --> z^2 + c
+def MandelbrotFunction (c, maxiter):
+    iterations = 0
+    z = complex(0,0)
+    for iteration in range(maxiter):
+        if abs(z) > 4:
+            break
+        iterations += 1
+        z = (z*z) + c
+    return iterations
+
+
+def CreateComplexPlain (maxIterations):
+    #set the location and size of the complex plane rectangle
+    xvalues = np.linspace(-0.22, -0.21, 1000)
+    yvalues = np.linspace(-0.70, -0.69, 1000)
+
+    #size of these lists of x and y values
+    xlen = len(xvalues)
+    ylen = len(yvalues)
+
+    #creates a 2D array, then creates each point, based off the xlen and ylen lists
+    atlas = np.empty((xlen,ylen))
+    for ix in range(xlen): #iterating over each complex point (x,y)
+        for iy in range(ylen):
+
+            cx = xvalues[ix]
+            cy = yvalues[iy]
+            c = complex(cx, cy)
+
+            #testing iterations per complex point c
+            atlas[ix,iy] = MandelbrotFunction(c,maxIterations)
+    return atlas
+
+def Main():
+    maxIterations = 40; #number of iterations until considered divergent
+    plt.figure(figsize = (18,18))
+    #myFractal = CreateComplexPlain(maxIterations)
+    plt.imshow(CreateComplexPlain(maxItedrations).T, interpolation = "nearest")
+    plt.show
+
+#--------------------------------------------------------------------    
+Main()
+        
